19aaf515321a8e3139978958fd0f5362a49490da
youssef <youssefhiro2019@gmail.com>
2024-05-11 02:03:58 +0100

    Aide Pour Commencer -1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Lahbari Routes 
+
+
+// Ghafir Routes
+
+
+// Kablaoui Routes
+
+
+// Zarria Routes
+
+
+// Moukhtari Routes
+
